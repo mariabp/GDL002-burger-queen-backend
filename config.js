659fe23fa5416a50bd5005dc06523d3061084a62
Maria Bolaños <mariadlp@gmail.com>
2019-06-18 18:19:57 -0500

    Connecting to my db

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.port = process.argv[2] || process.env.PORT || 8080;
-exports.mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/default';
+exports.mongoUrl = process.env.MONGO_URL || 'mongodb+srv://mariabp:mariabolanos89@my-first-cluster-732oh.mongodb.net/test?retryWrites=true&w=majority';
 exports.secret = process.env.JWT_SECRET || 'xxxxxxxx'; // JWT secret
 exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@localhost';
 exports.adminPassword = process.env.ADMIN_PASSWORD || 'changeme';
