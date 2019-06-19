8698d57fa534a306aa9bdb8425171e5b58b34e4d
Maria Bolaños <mariadlp@gmail.com>
2019-06-19 17:18:14 -0500

    Configuration with my database in mongo is working :)

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.port = process.argv[2] || process.env.PORT || 8080;
-exports.mongoUrl = process.env.MONGO_URL || 'mongodb+srv://mariabp:mariabolanos89@my-first-cluster-732oh.mongodb.net/test?retryWrites=true&w=majority';
-exports.secret = process.env.JWT_SECRET || 'xxxxxxxx'; // JWT secret
+exports.mongoUrl = process.env.MONGO_URL || 'mongodb+srv://mariabp:mariabolanos89@burger-queen-732oh.mongodb.net/burger-queen?retryWrites=true&w=majority';
+exports.secret = process.env.JWT_SECRET || 'burgerqueen2019'; // JWT secret
 exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@localhost';
-exports.adminPassword = process.env.ADMIN_PASSWORD || 'changeme';
+exports.adminPassword = process.env.ADMIN_PASSWORD || 'dontchangeme';
